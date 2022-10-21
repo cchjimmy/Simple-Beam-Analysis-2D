09d02150f3820e3a4338a98add64a586dd1b4d6d
cchjimmy <himhimjimmy@yahoo.com.hk>
2022-10-22 00:35:02 +0100

    changed SBA to SBA2D

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import SBA from './SBA.js';
+import SBA2D from './SBA2D.js';
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
@@ -11,5 +11,5 @@ function main() {
 
   // evalBeam(ctx, new Beam({'simply-supported': true, cantilever: false}, 10, 10, [new Force(0, -100000, 5)]));
   // evalBeam(ctx, new Beam({'simply-supported': true}, 12, 1, [new Force(0, -36000, 10)], [new DistForce(()=>-8000, 5, 0, 6)]));
-  SBA.evalBeam(ctx, new SBA.Beam({'simply-supported':true}, 10, 1, [new SBA.Force(0, -10000, 8)], [new SBA.DistForce(()=>-20000, 2, 0, 4)]))
+  SBA2D.evalBeam(ctx, new SBA2D.Beam({'simply-supported':true}, 10, 1, [new SBA2D.Force(0, -10000, 8)], [new SBA2D.DistForce(()=>-20000, 2, 0, 4)]))
 }
